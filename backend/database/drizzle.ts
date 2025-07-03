2fb147a07348c4ff5e746a966ab6d11fdf652371
heng sophavong <hengsophavong@gmail.com>
2025-07-04 00:22:00 +0700

    Update DB

--- backend/database/drizzle.ts
+++ backend/database/drizzle.ts
@@ -7,7 +7,8 @@ dotenv.config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
+  // Remove or comment out the ssl property for local Postgres!
+  // ssl: { rejectUnauthorized: false },
 });
 
 export const db = drizzle(pool, { schema: { usersTable, students, classrooms, classroomStudents,  reportCards, reportCardScores } });
